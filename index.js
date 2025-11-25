827ac71d042c85b77d926bbf2d8b545d0b1c7918
Omar <l25070062@valladolid.tecnm.mx>
2025-11-25 12:51:51 -0600

    Agregar un programa que salude al usuario y explique conceptos básicos de git

---
+++ index.js
@@ -0,0 +1,8 @@
+// Escribe un programa que salude al usuario por consola
+console.log("Hola, usuario!");
+//¿Qué es un commit y por qué es importante?"
+// Un commit es una instantánea de los cambios realizados en el código fuente de un proyecto. Es importante porque permite llevar un registro de las modificaciones, facilita la colaboración entre desarrolladores y ayuda a revertir cambios si es necesario.
+//"¿Cuál es la diferencia entre git add y git commit?"
+// git add se utiliza para agregar cambios específicos al área de preparación (staging area), mientras que git commit guarda esos cambios preparados en el historial del repositorio con un mensaje descriptivo.
+//"Explícame qué hace el comando git push".
+// El comando git push se utiliza para subir los commits locales a un repositorio remoto, permitiendo que otros colaboradores accedan a los cambios realizados en el proyecto.
